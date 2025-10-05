ae45ba2b89535cf7bd205542fcfcc5669508ad72
p4n1k0 <gabre.tarick@gmail.com>
2025-10-05 19:22:05 -0300

    tabuleiro completo e navios diagonais

--- batalha/naval.c
+++ batalha/naval.c
@@ -5,7 +5,7 @@ int main()
     int tabuleiro[10][10];
     int i, j;
 
-    // Inicializa tudo com 0 (água)
+    // --- Inicializa o tabuleiro com 0 (água) ---
     for (i = 0; i < 10; i++)
     {
         for (j = 0; j < 10; j++)
@@ -14,17 +14,36 @@ int main()
         }
     }
 
-    // Posiciona um navio horizontal (linha 2, colunas 3, 4 e 5)
-    tabuleiro[2][3] = 3;
-    tabuleiro[2][4] = 3;
-    tabuleiro[2][5] = 3;
+    // --- Navio 1 (horizontal) ---
+    // Linha 2, colunas 1 a 3
+    for (j = 1; j <= 3; j++)
+    {
+        tabuleiro[2][j] = 3;
+    }
 
-    // Posiciona um navio vertical (coluna 7, linhas 5, 6 e 7)
-    tabuleiro[5][7] = 3;
-    tabuleiro[6][7] = 3;
-    tabuleiro[7][7] = 3;
+    // --- Navio 2 (vertical) ---
+    // Coluna 6, linhas 5 a 7
+    for (i = 5; i <= 7; i++)
+    {
+        tabuleiro[i][6] = 3;
+    }
 
-    // Mostra o tabuleiro
+    // --- Navio 3 (diagonal principal: linha e coluna aumentam juntas) ---
+    // Começa em (0,0)
+    for (i = 0; i < 3; i++)
+    {
+        tabuleiro[i][i] = 3;
+    }
+
+    // --- Navio 4 (diagonal secundária: linha aumenta, coluna diminui) ---
+    // Começa em (4,9)
+    for (i = 0; i < 3; i++)
+    {
+        tabuleiro[4 + i][9 - i] = 3;
+    }
+
+    // --- Exibe o tabuleiro ---
+    printf("\n=== TABULEIRO BATALHA NAVAL ===\n\n");
     for (i = 0; i < 10; i++)
     {
         for (j = 0; j < 10; j++)
